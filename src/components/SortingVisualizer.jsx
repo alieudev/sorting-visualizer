228e7ad63ac49f84909305c09b0f319f0c65b847
Alieu Baldeh <alieu.dev@gmail.com>
2021-07-25 20:42:27 -0400

    changed state

--- src/components/SortingVisualizer.jsx
+++ src/components/SortingVisualizer.jsx
@@ -1,19 +1,26 @@
+import {useState} from 'react'
 import "./sortingvisualizer.css"
-function SortingVisualizer(){
-    const sortingNums = []
 
-    for (let i =0; i<140; i++){
-        sortingNums.push(Math.floor((Math.random() * 600)+ 10))
+function SortingVisualizer(){
+    const [newArray, setNewArray] = useState(generateNewArray);
+
+    function generateNewArray(){
+        const sortingNums = []
+        for (let i =0; i<140; i++){
+        sortingNums.push(Math.floor((Math.random() * 600)+ 10))}
     }
+    newArray = setNewArray(generateNewArray());
+
     return(
         <div className='array-container'>
-            {sortingNums.map((num, idx) => {
+        {sortingNums.map((num, idx) => {
                 return(<div className='array-bar' 
                 key={idx}
                 style={{height:`${num}px`, 
                 backgroundColor:'#ffe5d9' }}>
                 </div>)
             })}
+            <button onClick={()=> generateNewArray }>Generate New Array</button>
         </div>
     )
 }
